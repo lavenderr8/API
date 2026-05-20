1975696d4e1c700a624f60334a9334d10cbef4a2
lavander8 <lavander8@rambler.su>
2026-05-20 13:18:18 +0300

    Метод для проверки статус кода

--- tests/test_google_maps_api.py
+++ tests/test_google_maps_api.py
@@ -1,4 +1,3 @@
-from requests import Response
 from utils.api import GoogleMapsApi
 from utils.checking import Checking
 
@@ -11,20 +10,17 @@ class TestCreatePlace:
         print("\nМетод POST")
 
         # Вызов POST метода
-        result_post: Response = GoogleMapsApi.create_new_place()
+        result_post = GoogleMapsApi.create_new_place()
 
         # Проверка POST
         Checking.check_status_code(result_post, 200)
 
-        # Проверка обязательных полей POST
-        Checking.check_json_token(
-            result_post,
-            expected_value=['status', 'place_id', 'scope', 'reference', 'id']
-        )
-
         # Проверка значения поля status
         check_post = result_post.json()
-        assert check_post["status"] == "OK"
+
+        assert check_post["status"] == "OK", \
+            "Неверное значение поля status"
+
         print("POST message корректный")
 
     # Тест GET метода
@@ -33,7 +29,7 @@ class TestCreatePlace:
         print("\nМетод POST")
 
         # Вызов POST метода
-        result_post: Response = GoogleMapsApi.create_new_place()
+        result_post = GoogleMapsApi.create_new_place()
 
         # Получаем place_id из ответа POST
         place_id = result_post.json()["place_id"]
@@ -42,17 +38,10 @@ class TestCreatePlace:
         print("\nМетод GET")
 
         # Вызов GET метода
-        result_get: Response = GoogleMapsApi.get_new_place(place_id)
+        result_get = GoogleMapsApi.get_new_place(place_id)
 
         # Проверка GET
         Checking.check_status_code(result_get, 200)
-
-        # Проверка обязательных полей GET
-        Checking.check_json_token(
-            result_get,
-            expected_value=['location', 'accuracy', 'name', 'phone_number',
-                            'address', 'types', 'website', 'language']
-        )
 
     # Тест PUT метода
     def test_put_new_place(self):
@@ -60,7 +49,7 @@ class TestCreatePlace:
         print("\nМетод POST")
 
         # Вызов POST метода
-        result_post: Response = GoogleMapsApi.create_new_place()
+        result_post = GoogleMapsApi.create_new_place()
 
         # Получаем place_id из ответа POST
         place_id = result_post.json()["place_id"]
@@ -69,20 +58,17 @@ class TestCreatePlace:
         print("\nМетод PUT")
 
         # Вызов PUT метода
-        result_put: Response = GoogleMapsApi.put_new_place(place_id)
+        result_put = GoogleMapsApi.put_new_place(place_id)
 
         # Проверка PUT
         Checking.check_status_code(result_put, 200)
 
-        # Проверка обязательных полей PUT
-        Checking.check_json_token(
-            result_put,
-            expected_value=['msg']
-        )
-
         # Проверка сообщения PUT
         check_put = result_put.json()
-        assert check_put["msg"] == "Address successfully updated"
+
+        assert check_put["msg"] == "Address successfully updated", \
+            "Неверное значение поля msg"
+
         print("PUT message корректный")
 
     # Тест DELETE метода
@@ -91,7 +77,7 @@ class TestCreatePlace:
         print("\nМетод POST")
 
         # Вызов POST метода
-        result_post: Response = GoogleMapsApi.create_new_place()
+        result_post = GoogleMapsApi.create_new_place()
 
         # Получаем place_id из ответа POST
         place_id = result_post.json()["place_id"]
@@ -100,21 +86,15 @@ class TestCreatePlace:
         print("\nМетод DELETE")
 
         # Вызов DELETE метода
-        result_delete: Response = GoogleMapsApi.delete_new_place(place_id)
+        result_delete = GoogleMapsApi.delete_new_place(place_id)
 
         # Проверка DELETE
         Checking.check_status_code(result_delete, 200)
 
-        # Проверка обязательных полей DELETE
-        Checking.check_json_token(
-            result_delete,
-            expected_value=['status']
-        )
-
         # Проверка значения поля status
         check_delete = result_delete.json()
-        assert check_delete["status"] == "OK"
-        print("DELETE message корректный")
 
-    # Вывод об успешном завершении теста
-    print("\nТестирование создания, изменения и удаления новой локации прошло успешно!")
+        assert check_delete["status"] == "OK", \
+            "Неверное значение поля status"
+
+        print("DELETE message корректный")
